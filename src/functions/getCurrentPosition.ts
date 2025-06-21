d60fd90e68a0ad1c444ef11b73fea8738a48cbe1
Hodaka-A <anxisuigao@gmail.com>
2025-06-21 20:17:31 +0900

    関数とモジュールの反映

---
+++ src/functions/getCurrentPosition.ts
@@ -0,0 +1,25 @@
+export type LatLng = {
+  lat: number;
+  lng: number;
+};
+
+export const getCurrentLocation = (): Promise<LatLng> => {
+  return new Promise((resolve, reject) => {
+    const success = (pos)=> {
+      const lat = pos.coords.latitude;
+      const lng = pos.coords.longitude;
+      resolve({ lat, lng });
+    };
+
+    const error = (err: GeolocationPositionError) => {
+      console.error("位置情報取得エラー:", err);
+      reject(new Error("位置情報の取得に失敗しました: " + err.message));
+    };
+
+    if (!navigator.geolocation) {
+      reject(new Error("このブラウザでは位置情報がサポートされていません"));
+    } else {
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  });
+};
